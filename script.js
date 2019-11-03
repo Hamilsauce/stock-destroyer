fd39954dcf9136ac119279e26bac1f2af9514cfd
jacobwilsonhamilton <jacobwilsonhamilton@gmail.com>
2019-11-03 05:15:50 -0600

    all the updates

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 let apiKey;
 let symbol;
 const hamKey = 'F1P4FC58217NPJEL;'
+let records = [];
 
 document.querySelector('#stock-form').addEventListener('submit', e => {
+    e.preventDefault();
     let dataMessage = document.querySelector('#data-display-message');
     let dataDisplay = document.querySelector('#divContents');
 
-    e.preventDefault();
     getAlphaVantagedata();
     dataDisplay.innerHTML = '';
     dataMessage.innerHTML = 'Waiting for data...'
-
 });
 
-//run  init function defined immediately below upon loading
+document.querySelector('#find-button').addEventListener('click', e => {
+    const dateInput = document.querySelector('#date-input').value;
+    findDate(dateInput);
+});
+
+//run in it function defined immediately below upon loading
 init();
 
 function init() {
     apiKey = localStorage.getItem('apiKey');
-    // inpApiKey.value = apiKey ? apiKey : '';
     apiKey = hamKey;
 }
 
 /*
- Part 1: get data from storage, user; validate data and construct query string/url for http request
+Part 1: get data from storage, user; validate data and construct query string/url for http request
 Part 2: Make the call
 Part 3: make someting useful out of returned data, display it on screen
 */
@@ -33,24 +37,16 @@ function getAlphaVantagedata() {
     const type = selType.value;
     const interval = selInterval.value;
     let returnSymbol = '';
-    let records = [];
+    //  let records = [];
     let record = '';
     let htmlArray = [];
     symbol = inpSymbol.value;
+    const url =
+        `https://www.alphavantage.co/query?function=${func}&symbol=${symbol}&interval=${interval}&outputs=${size}&datatyp=${type}&apikey=${apiKey}`;
 
-    const demo = symbol === 'MSFT' && apiKey === 'demo' ? true : false;
-
-    if (demo === true) {
-        url =
-            'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=demo';
-    } else {
-
-        url = `https://www.alphavantage.co/query?function=${func}&symbol=${symbol}&interval=${interval}&outputs=${size}&datatyp=${type}&apikey=${apiKey}`;
-        console.log('url: ' + url);
-
-    }
-
-    request({ url: url })
+    request({
+        url: url
+    })
         .then(data => {
             let stockData = JSON.parse(data);
             let html = "";
@@ -58,30 +54,29 @@ function getAlphaVantagedata() {
             let dataMessage = document.querySelector('#data-display-message');
             let propNames = Object.keys(stockData);
 
-
-
             records = Object.entries(stockData[propNames[1]]);
 
             records.forEach(stock => {
                 let [entryName, entryDetails] = stock;
                 let record = Object.entries(stock);
-                // console.log('test', 'stock = ' + stock, 'entryName = ' + entryName, 'details = ' +
-                //     entryDetails);
+                let entryDate = new Date(entryName).toDateString();
 
                 let printData = rec => {
                     let recOut = [];
                     rec.forEach(recProp => {
-                        let line = /*html*/`<li style="text-align: center;"><p>${recProp[0].slice(3)}:</p> ${recProp[1]}</li>`;
+                        let line = /*html*/ `<li style="text-align: center;">
+                            <p>${recProp[0].slice(3)}:</p> ${recProp[1]}
+                        </li>`;
                         recOut.push(line);
                     });
 
                     return recOut.join('');
                 };
-                let recordHTML = /*html*/`
-                    <div class="record">
-                        <div class="entry-name-container">${entryName}</div>
-                        <ul class="entry-details-list">${printData(Object.entries(entryDetails))}</ul>
-                    </div>`;
+                let recordHTML = /*html*/ `
+                        <div class="record">
+                            <div class="entry-name-container">${entryDate}</div>
+                            <ul class="entry-details-list">${printData(Object.entries(entryDetails))}</ul>
+                        </div>`;
                 htmlArray.push(recordHTML);
             });
             // console.log('symbol = ' + Object.entries(stockData["Meta Data"]["2. Symbol"]));
@@ -139,11 +134,10 @@ function setInterval() {
 
 function saveDataToFile() {
     const data = document.querySelector('#divContents');
-    const blob = new Blob([data.innerHTML]);
-
+    const blob = new Blob([records]);
     let a = document.body.appendChild(document.createElement('a'));
 
-    a.href = window.url.createObjecturl(blob);
+    a.href = window.URL.createObjectURL(blob);
     a.download = symbol + '.txt';
     a.click();
     a = null;
@@ -156,4 +150,48 @@ function storeApiKey() {
 
 function getStorage() {
     apiKey = localStorage.getItem('apiKey');
+}
+
+const findDate = dateStr => {
+    let targetDate = new Date(dateStr).toDateString();
+    let currDate;
+    let dateResult = {};
+
+    let dateCompare = records.some(rec => {
+        currDate = new Date(rec[0]).toDateString();
+        if (targetDate == currDate) {
+            dateResult = rec[1];
+        }
+        return targetDate == currDate;
+    });
+    console.log(dateCompare);
+    writeFoundData(currDate, dateCompare, dateResult);
+}
+
+const writeFoundData = (date, dateFound, results) => {
+    const contentDisplay = document.querySelector('#search-results');
+    const dataMessage = document.querySelector('#data-display-message');
+    let message = '';
+    let dateArray = Object.entries(results);
+    let htmlOutput = [];
+
+    if (dateFound == false) {
+        message = `No results found for ${symbol} on ${date}. Fail!`;
+        dataMessage.innerHTML = message;
+        htmlOutput = 'Nope';
+        return;
+    } else {
+        message = `Showing results for ${symbol} on ${date}.`;
+        dateArray.forEach(el => {
+            let [dataKey, dataValue] = el;
+            let pairString = `<li class="data-point">
+                            <p style="font-weight: bold;">${dataKey.slice(3)}</p>
+                            <p>${dataValue}</p>
+                        </li>`;
+
+            htmlOutput.push(pairString);
+        });
+        dataMessage.innerHTML = message;
+        contentDisplay.innerHTML = `<ul class="date-result-list">${htmlOutput.join('')}</ul>`;
+    }
 }
